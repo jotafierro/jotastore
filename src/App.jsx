2d698ba042b0a437d3ba497d73a69da82a9a4e53
Jonathan Fierro <jotafierro.tech@gmail.com>
2023-02-10 23:14:29 -0300

    add ItemListContainer with greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.scss'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import Navbar from './components/Navbar/Navbar'
 
 const App = () => (
   <>
     <Navbar />
+    <ItemListContainer greeting={'Welcome my store!'}/>
   </>
 )
 
